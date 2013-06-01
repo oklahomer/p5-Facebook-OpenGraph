3d764a6959247feca4a255a2e6f7aa56c62b62fc
Oklahomer <go.hagiwara@facebook.com>
2013-06-02 00:58:19 +0900

    updated VERSION

--- lib/Facebook/OpenGraph.pm
+++ lib/Facebook/OpenGraph.pm
@@ -12,7 +12,7 @@ use Digest::SHA qw(hmac_sha256 hmac_sha256_hex);
 use MIME::Base64::URLSafe qw(urlsafe_b64decode);
 use Scalar::Util qw(blessed);
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub new {
     my $class = shift;
@@ -518,7 +518,7 @@ Facebook::OpenGraph - Simple way to handle Facebook's Graph API.
 
 =head1 VERSION
 
-This is Facebook::OpenGraph version 0.04
+This is Facebook::OpenGraph version 0.05
 
 =head1 SYNOPSIS
     
