8dbc3b551e7ae136fb0e809e08cf0d7fd50f74fe
Oklahomer <hagiwara.go@gmail.com>
2019-04-30 09:21:07 +0900

    Checking in changes prior to tagging of version 1.25.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ef382be..b68dc0e 100644
    --- a/Changes
    +++ b/Changes
    @@ -19,6 +19,10 @@ https://developers.facebook.com/bugs/597779113651383/
    
     {{$NEXT}}
    
    +1.25 2019-04-30T00:20:02Z
    +    - Fix version comparison. Thanks to ksmadsen.
    +      https://github.com/oklahomer/p5-Facebook-OpenGraph/pull/21
    +
     1.24 2018-04-05T02:17:07Z
         - Fix API response version comparison. Thanks to chakkit.
           https://github.com/oklahomer/p5-Facebook-OpenGraph/issues/18

--- lib/Facebook/OpenGraph.pm
+++ lib/Facebook/OpenGraph.pm
@@ -14,7 +14,7 @@ use Digest::SHA qw(hmac_sha256 hmac_sha256_hex);
 use MIME::Base64::URLSafe qw(urlsafe_b64decode);
 use Scalar::Util qw(blessed);
 
-our $VERSION = '1.24';
+our $VERSION = '1.25';
 
 sub new {
     my $class = shift;
@@ -683,7 +683,7 @@ Facebook::OpenGraph - Simple way to handle Facebook's Graph API.
 
 =head1 VERSION
 
-This is Facebook::OpenGraph version 1.24
+This is Facebook::OpenGraph version 1.25
 
 =head1 SYNOPSIS
 
